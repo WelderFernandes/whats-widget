b70f09939786e09f121b82e17ebc5c4829de2944
Welder n. Fernandes <welderx3@gmail.com>
2022-03-21 15:44:57 -0300

    teste widget

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const WhatsWidget = document.querySelectorAll(".whatsapp_widget");
+
+WhatsWidget.forEach((Div) => {
+	ReactDOM.render(
+		<React.StrictMode>
+			{/* <App /> */}
+			<App domElement={Div} />
+		</React.StrictMode>,
+		Div
+	);
+});
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
